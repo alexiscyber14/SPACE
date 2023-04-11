180595c9d97c3bf6ca04d9ecf930ede0e3ce2e96
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2023-04-11 16:31:42 +0530

    basic setup, fetch and render

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { missionsReducer } from './missions/missions';
 
 export const store = configureStore({
   reducer: {
+    missions: missionsReducer,
   },
 });
+
+export default store;
