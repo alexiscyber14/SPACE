1b4e2d7a8d8d0f23356ed05264c662aa898a36f0
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2023-04-11 16:12:11 +0530

    Basic strucute, fetch and render

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import '../App.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Table from 'react-bootstrap/Table';
+import { fetchMissonsAction } from '../redux/missions/missions';
+import MissionItem from './MissionItem';
 
-const MissionsList = () => (
-  <div>
-    <h1>mission page</h1>
-  </div>
-);
+const Mission = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMissonsAction());
+  }, [dispatch]);
 
-export default MissionsList;
+  const mission = useSelector((state) => state.missions);
+
+  return (
+    <Table striped bordered hover responsive="sm">
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th aria-label="member" />
+        </tr>
+      </thead>
+      <tbody>
+        {mission.map((item) => (
+          <MissionItem
+            key={item.mission_id}
+            missionName={item.mission_name}
+            missionDesc={item.description}
+          />
+        ))}
+      </tbody>
+    </Table>
+
+  );
+};
+
+export default Mission;
