c8406e5e70cd34e71798c4289837d96ef3807a11
alexiscyber14 <142sledgehammer14@gmail.com>
2023-04-11 11:02:05 +0100

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-// import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
+import MissionsList from './components/Missions';
+import Rockets from './components/Rockets';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <h1>main branch</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<MissionsList />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
