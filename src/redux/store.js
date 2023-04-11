45dd9df7205d9eac5f659ccc03200472b8eb2ca9
alexiscyber14 <142sledgehammer14@gmail.com>
2023-04-11 14:54:27 +0100

    add fetch data and list render functionality

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { missionsReducer } from './missions/missions';
+import rocketsReducer from './rockets/rocketsSlice';
 
 export const store = configureStore({
   reducer: {
     missions: missionsReducer,
+    rockets: rocketsReducer,
   },
 });
 
