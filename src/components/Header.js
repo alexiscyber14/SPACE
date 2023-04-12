ea87a3a0eadda873040e463c8a92189c5c56c2f0
alexiscyber14 <142sledgehammer14@gmail.com>
2023-04-12 11:57:12 +0100

    add profile rockets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
+
 import '../App.css';
 
 const Header = () => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
 
   useEffect(() => {
     if (window.location.pathname === '/missions') {
@@ -28,9 +37,21 @@ const Header = () => {
         </div>
         <div>
           <ul>
-            <li className="list-item"><a href="/" className={`routing ${activeIndex === 0 ? 'activated' : ''}`} onClick={(e) => handleClick(0, e)}>Rockets</a></li>
-            <li><a href="/missions" className={`routing ${activeIndex === 1 ? 'activated' : ''}`} onClick={(e) => handleClick(1, e)}>Missions</a></li>
-            <li><a href="/profile" className={`routing ${activeIndex === 2 ? 'activated' : ''}`} onClick={(e) => handleClick(1, e)}>My Profile</a></li>
+            <li>
+              <NavLink to="/" className={`routing ${activeIndex === 0 ? 'activated' : ''}`} onClick={(e) => handleClick(0, e)}>
+                Rockets
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/missions" className={`routing ${activeIndex === 1 ? 'activated' : ''}`} onClick={(e) => handleClick(1, e)}>
+                Missions
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/profile" className={`routing ${activeIndex === 2 ? 'activated' : ''}`} onClick={(e) => handleClick(2, e)}>
+                Profile
+              </NavLink>
+            </li>
           </ul>
         </div>
       </nav>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import '../App.css';
+import { useSelector } from 'react-redux';
 
-const Profile = () => (
-  <div>
-    <h1>Profile page</h1>
-  </div>
-);
+const Profile = () => {
+  const reservedRockets = useSelector((state) => state.rockets.filter((rocket) => rocket.reserved));
+  console.log('reserved', reservedRockets);
+
+  return (
+    <div className="main">
+      <div className="homeContent">
+        <div className="profile">
+          <div>
+            <h2>My Missions</h2>
+          </div>
+          <div>
+            <h2>My Rockets</h2>
+            <ul>
+              {reservedRockets.map((rocket) => (
+                <li key={rocket.id}>
+                  <span>{rocket.name}</span>
+                  <span>{rocket.id}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
