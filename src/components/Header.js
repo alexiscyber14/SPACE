4ba4698037dbc12d26d92d2a0bd75080d730836b
alexiscyber14 <142sledgehammer14@gmail.com>
2023-04-13 08:41:38 +0100

    correct linter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import Planet from '../planet.png';
 import '../App.css';
 
 const Header = () => {
@@ -22,7 +23,7 @@ const Header = () => {
       <nav>
         <div className="logo-container">
           <div className="logo">
-            <i className="fa fa-plane" id="lock" />
+            <img src={Planet} alt="logo_image" id="lock" />
             <a href="/" id="logo-link">Space Traveler&lsquo;s Hub</a>
             <span className="trade">tm</span>
           </div>
